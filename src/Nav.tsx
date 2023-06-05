02f5d9d6bad098abf67288c187c48d2670500908
Chris Albert <christopher.albert@mlb.com>
2023-06-04 19:09:43 -0700

    Adding drawer for nav

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import {
   AppBar,
   Box,
-  Toolbar, Grid,
+  Toolbar, Grid, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText
 } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 import {MidiInputComponent} from "./components/MidiInputComponent";
 import {MidiInput, WindowMidi} from "./midi/WindowMidi";
+import { NavLink } from "react-router-dom"
+import {NavLinkItem} from "./components/NavLinkItem";
 
 export type NavProps = {
   midi: WindowMidi | undefined
@@ -16,11 +20,46 @@ export const Nav: React.FC<NavProps> = ({
     midi,
     onInputSelect
 }) => {
+
+  const [menuOpen, setMenuOpen] = React.useState(false)
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
+        <Drawer
+          anchor='left'
+          open={menuOpen}
+          onClose={() => setMenuOpen(false)}
+        >
+          <Box
+            role="presentation"
+            sx={{width: 250}}
+          >
+            <List>
+              <NavLinkItem
+                path='/'
+                label='Home'
+                onClick={() => setMenuOpen(false)}
+              />
+              <NavLinkItem
+                path='/monitor'
+                label='MIDI Monitor'
+                onClick={() => setMenuOpen(false)}
+              />
+            </List>
+          </Box>
+        </Drawer>
         <Toolbar>
-
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 1 }}
+            onClick={() => setMenuOpen(true)}
+          >
+            <MenuIcon />
+          </IconButton>
           <Grid container >
             <Grid item xs={4}>
               <MidiInputComponent
--- src/components/ProjectComponent.tsx
+++ src/components/ProjectComponent.tsx
@@ -6,7 +6,7 @@ export const ProjectComponent: React.FC<ProjectComponentProps> = ({
 
 }) => {
 
-  
+
   return (
     <>ProjectComponent</>
   )
