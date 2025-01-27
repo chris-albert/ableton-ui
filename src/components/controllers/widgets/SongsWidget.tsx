aa6fa43e07cadf57a6ba66a8a2fb4fdc58af45da
Chris Albert <chris.albert@siriusxm.com>
2025-01-26 20:15:33 -0800

    songs

--- src/components/controllers/widgets/SongsWidget.tsx
+++ src/components/controllers/widgets/SongsWidget.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { MidiTarget } from '../../../midi/MidiTarget'
 import { ProjectHooks } from '../../../hooks/ProjectHooks'
 import { useActiveClip } from '../../../hooks/ActiveClipHook'
+import _ from 'lodash'
+import { NavigateableClip } from '../../../model/UIStateDisplay'
+import { TX_MESSAGE } from '../../../model/AbletonUIMessage'
+import { Midi } from '../../../midi/GlobalMidi'
 
 type SongsWidgetProps = {
   targets: Array<MidiTarget>
@@ -9,9 +13,31 @@ type SongsWidgetProps = {
 }
 
 export const SongsWidget: React.FC<SongsWidgetProps> = ({ targets, trackName }) => {
-  const arranmement = ProjectHooks.useArrangement()
+  const dawEmitter = Midi.useDawEmitter()
+  const arrangement = ProjectHooks.useArrangement()
   const track = ProjectHooks.useTrack(trackName)
   const activeClip = useActiveClip(track)
+
+  const cueHash = React.useMemo(() => {
+    return _.fromPairs(_.map(arrangement.cues, (cue) => [cue.time, cue]))
+  }, [arrangement.cues])
+
+  const clips = React.useMemo(() => {
+    const tmpClips: Array<NavigateableClip> = []
+    track.clips.forEach((clip) => {
+      if (clip.type === 'real') {
+        const cue = _.get(cueHash, clip.startTime, undefined)
+        if (cue !== undefined) {
+          tmpClips.push({ clip, cue })
+        }
+      }
+    })
+    return tmpClips
+  }, [track, cueHash])
+
+  const onClick = (clip: NavigateableClip) => {
+    dawEmitter.send(TX_MESSAGE.jumpToCue(clip.cue.index))
+  }
 
   return <></>
 }
